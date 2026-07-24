0a778a3890e1ef4250f29d25cd4d2efd219c4e2f
BlackedProxy <BlackedProxy@users.noreply.github.com>
2026-07-24 22:49:56 +0200

    protocol: pin client version 7.60, accept legacy items.otb

--- src/definitions.h
+++ src/definitions.h
@@ -8,9 +8,9 @@ static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
 static constexpr auto STATUS_SERVER_VERSION = "1.6";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
 
-static constexpr auto CLIENT_VERSION_MIN = 1310;
-static constexpr auto CLIENT_VERSION_MAX = 1311;
-static constexpr auto CLIENT_VERSION_STR = "13.10";
+static constexpr auto CLIENT_VERSION_MIN = 760;
+static constexpr auto CLIENT_VERSION_MAX = 760;
+static constexpr auto CLIENT_VERSION_STR = "7.60";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
 static constexpr auto AUTHENTICATOR_PERIOD = 30U;
